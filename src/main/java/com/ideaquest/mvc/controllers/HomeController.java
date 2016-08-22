80e87d201dbab3322511a3c4058ad11e9a52f31c
Marius Cealera <marius.cealera@gmail.com>
2016-08-22 13:43:18 +0300

    IdeaQuest initial commit

---
+++ src/main/java/com/ideaquest/mvc/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.ideaquest.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping("/home")
+	public String goHome(){
+		
+		return "home";
+	}
+
+
+}
